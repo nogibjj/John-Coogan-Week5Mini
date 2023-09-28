95923bedea7824c1006adf6f68565cfafb96efab
johncoogan53 <141885470+johncoogan53@users.noreply.github.com>
2023-09-28 13:11:38 +0000

    quick changed before going local

--- mylib/query.py
+++ mylib/query.py
@@ -3,11 +3,11 @@
 import sqlite3
 
 
-def query():
+def query( query ):
     """Query the database for the top 5 rows of the GroceryDB table"""
     conn = sqlite3.connect("GroceryDB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM GroceryDB")
+    cursor.execute(query)
     print("Top 5 rows of the GroceryDB table:")
     print(cursor.fetchall())
     conn.close()
