5f387754f10acf7f8b5cad4b5049a5c4576487b9
johncoogan53 <john.h.coogan@protonmail.com>
2023-10-01 09:49:52 -0400

    updated a test query

--- test_main.py
+++ test_main.py
@@ -6,7 +6,8 @@ Test goes here
 from mylib import extract, query,transform_load
 
 
-def test_query():
+def test_query("SELECT Name FROM GameDB WHERE User_Score = (SELECT MAX(User_Score) FROM GameDB))"):
+    
     pass
 def test_extract():
     pass
